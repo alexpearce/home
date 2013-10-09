dac8c54f59bac36fa601ff796dd6881b3eab3070
Alex Pearce <alex@alexpearce.me>
2013-10-09 17:41:11 +0100

    Move from GitHub Pages to Mina deployment on mu.

---
+++ config/deploy.rb
@@ -0,0 +1,41 @@
+require "mina/bundler"
+require "mina/git"
+require "mina/rbenv"
+
+set :app, "alexpearce.me"
+
+set :user, "deploy"
+set :port, "19808"
+set :domain, "192.249.56.87"
+set :deploy_to, "/home/deploy/apps/#{app}"
+
+set :repository, "git://github.com/alexpearce/alexpearce.github.com.git"
+set :branch, "master"
+
+set :shared_paths, []
+
+task :setup => :environment do
+  invoke :link
+end
+
+task :link do
+  queue "ln -sf #{deploy_to}/#{current_path}/config/nginx.conf /opt/nginx/sites-enabled/#{app}.conf"
+end
+
+task :unlink do
+  queue "rm /opt/nginx/sites-enabled/#{app}.conf"
+end
+
+task :environment do
+  invoke :"rbenv:load"
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+    invoke :"git:clone"
+    invoke :"bundle:install"
+    queue "#{bundle_bin} exec jekyll --no-auto"
+  end
+end
+
