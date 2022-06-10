4b313f562a16bf7bb637dbef5abbfcba68bc58ae
Alex Pearce <alex@alexpearce.me>
2022-06-10 04:27:46 +0100

    Update flake.

--- flake.nix
+++ flake.nix
@@ -1,13 +1,17 @@
 {
+  description = "alexpearce.me website";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShell = pkgs.mkShell { buildInputs = [ pkgs.nodejs ]; };
-      });
+    flake-utils.lib.simpleFlake {
+      inherit self nixpkgs;
+      name = "alexpearce-me";
+      shell = { pkgs ? nixpkgs }:
+        pkgs.mkShell {
+          buildInputs = [ pkgs.nodejs ];
+        };
+    };
 }
