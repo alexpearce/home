132385fbbdbc6e3ae4e577ef9b88a7682652f757
Alex Pearwin <alex@pearwin.com>
2025-05-27 06:51:18 +0100

    Update Node and dependencies.

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "alex.pearwin.com website";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -17,8 +18,8 @@
       devShells = {
         default = pkgs.mkShell {
           buildInputs = [
-            pkgs.nodejs
-            pkgs.nodePackages.typescript-language-server
+            pkgs.nodePackages_latest.nodejs
+            pkgs.nodePackages_latest.typescript-language-server
           ];
           NODE_ENV = "development";
         };
