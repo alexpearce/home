971973198664aa6bbbdd5ca73ddd5f42ccf13ef1
Alex Pearwin <alex@pearwin.com>
2025-12-12 20:18:02 +0000

    Use nixfmt-tree.

--- flake.nix
+++ flake.nix
@@ -6,23 +6,28 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      formatter = pkgs.alejandra;
-      devShells = {
-        default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.nodePackages_latest.nodejs
-            pkgs.nodePackages_latest.typescript-language-server
-          ];
-          NODE_ENV = "development";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixfmt-tree;
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.nodePackages_latest.nodejs
+              pkgs.nodePackages_latest.typescript-language-server
+            ];
+            NODE_ENV = "development";
+          };
         };
-      };
-    });
+      }
+    );
 }
